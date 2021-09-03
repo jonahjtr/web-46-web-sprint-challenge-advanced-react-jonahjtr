c6ad441aa0337f7711c6a92167ce111d9f0f6775
jonahjtr <86381032+jonahjtr@users.noreply.github.com>
2021-09-03 11:11:16 -0500

    set state

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,17 +2,22 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+  // add state with a property called "plants" - initialize as an empty array ---done
+  state = {
+    plants: [],
+  };
 
   // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
+  //   - fetch data from the server endpoint - http://localhost:3333/plants  - ---done
+  //   - set the returned plants array to this.state.plants ---- done
 
   componentDidMount() {
-    axios.get('http://localhost:3333/plants')
-      .then(res => {
-        
+    axios.get("http://localhost:3333/plants").then((res) => {
+      console.log(res.data);
+      this.setState({
+        plants: res.data,
       });
+    });
   }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
