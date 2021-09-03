0fa0df66f262dd7c56505ede63cc7f2076006677
jonahjtr <86381032+jonahjtr@users.noreply.github.com>
2021-09-03 11:32:39 -0500

    created and implemented custom hooks

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -15,10 +16,10 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [value, setStoredValue] = useForm("no-key", initialValue);
 
   const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
+    setStoredValue({ ...value, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
@@ -34,7 +35,7 @@ const CheckoutForm = (props) => {
           First Name:
           <input
             name="firstName"
-            value={values.firstName}
+            value={value.firstName}
             onChange={handleChanges}
           />
         </label>
@@ -42,7 +43,7 @@ const CheckoutForm = (props) => {
           Last Name:
           <input
             name="lastName"
-            value={values.lastName}
+            value={value.lastName}
             onChange={handleChanges}
           />
         </label>
@@ -50,21 +51,21 @@ const CheckoutForm = (props) => {
           Address:
           <input
             name="address"
-            value={values.address}
+            value={value.address}
             onChange={handleChanges}
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" value={value.city} onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" value={value.state} onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zip" value={value.zip} onChange={handleChanges} />
         </label>
         <button>Checkout</button>
       </form>
@@ -78,11 +79,11 @@ const CheckoutForm = (props) => {
           <br />
           <br />
           <p>
-            {values.firstName} {values.lastName}
+            {value.firstName} {value.lastName}
           </p>
-          <p>{values.address}</p>
+          <p>{value.address}</p>
           <p>
-            {values.city}, {values.state} {values.zip}
+            {value.city}, {value.state} {value.zip}
           </p>
         </div>
       )}
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,22 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from "react";
+
+const useForm = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
+    if (localStorage.getItem(key)) {
+      return JSON.parse(localStorage.getItem(key));
+    } else {
+      localStorage.setItem(key, JSON.stringify(initialValue));
+      return initialValue;
+    }
+  });
+
+  const setStoredValue = (value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+    setValue(value);
+  };
+
+  return [value, setStoredValue];
+};
+
+export default useForm;
